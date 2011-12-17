347806d9585ec40eb17ff21a0bc1b786352c6cd8
Karl Voelker <ktvoelker@gmail.com>
2011-12-16 23:50:06 -0800

    Wrote base cases for the layout backtracking

--- Layout.hs
+++ Layout.hs
@@ -17,5 +17,13 @@ instance Puzzle Layout Network where
   solved net lay =
     nLineCount net == lLineCount lay
     && (Set.size $ lEdges $ head $ nLines net) == (length $ head $ lLines lay)
-  choices net lay = [] -- TODO
+  choices net lay =
+    if Set.size curNetLine == length curLayLine
+    then -- We've finished this line, so go on to the next.
+      choices net Layout { lLines = [] : lLines lay, lLineCount = lLineCount lay + 1 }
+    else -- We're in the middle of this line, so place the next edge.
+      [] -- TODO
+    where
+      curNetLine = lEdges $ head $ drop (nLineCount net - lLineCount lay) $ nLines net
+      curLayLine = head $ lLines lay
 
